13801b002017d23cac46d2f3c380cf1f656c25b1
vicentemarti31416 <vicentemarti31416@gmail.com>
2023-01-05 12:55:35 +0100

    Añadido config-server

---
+++ microservicio-config/src/main/java/vicente/marti/microservicioconfig/MicroservicioConfigApplication.java
@@ -0,0 +1,15 @@
+package vicente.marti.microservicioconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class MicroservicioConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioConfigApplication.class, args);
+	}
+
+}
