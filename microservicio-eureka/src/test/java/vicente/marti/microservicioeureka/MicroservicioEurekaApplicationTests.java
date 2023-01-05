d36841a70de7f4b244444d7a6c0de83a44134358
vicentemarti31416 <vicentemarti31416@gmail.com>
2023-01-05 16:45:36 +0100

    Añadido servidor eureka

---
+++ microservicio-eureka/src/test/java/vicente/marti/microservicioeureka/MicroservicioEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package vicente.marti.microservicioeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
